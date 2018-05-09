f492c9019edf0eff63b0671bfd1cf38324d09bd4
Dario Ferderber <dario.ferderber@hotmail.com>
2018-05-09 22:56:01 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,16 @@ if (style.styleSheet){
 head.appendChild(style);
 
 var headerContent = document.querySelectorAll('header .text-wrapper')[0];
-window.addEventListener('scroll', function(e){
+window.addEventListener('scroll', checkOffset);
+window.addEventListener('load', checkOffset);
+
+function checkOffset(){
   var scrollTop = (window.pageYOffset || document.documentElement.scrollTop) - (document.documentElement.clientTop || 0);
   if(!headerContent.classList.contains('is-hidden') && scrollTop > 100){
     headerContent.classList.add('is-hidden')
   }
   if(scrollTop < 100) headerContent.classList.remove('is-hidden')
-});
+}
 
 var scrollers = document.querySelectorAll('[data-scrollto]');
 
@@ -26,6 +29,20 @@ for(var i = 0; i < scrollers.length; i++){
   scrollers[i].addEventListener('click', function(e){
     var element = e.target.dataset.scrollto != undefined ? e.target.dataset.scrollto : e.target.parentElement.dataset.scrollto
     var top = element != 'top' ? document.querySelectorAll(element)[0].getBoundingClientRect().top + window.scrollY - 100 : 0;
+
+    if(document.querySelectorAll(element)[0].dataset.offset = 'half'){
+      top -= window.innerHeight/4;
+    }
     window.scroll({top: top, left: 0, behavior: 'smooth' });
   })
 }
+
+
+var video = document.querySelectorAll('video')[0];
+video.play();
+var interval = setInterval(function(){
+  if(video.readyState === 4) {
+    document.body.classList.remove('loading')
+    clearInterval(interval);
+  }
+}, 100)
